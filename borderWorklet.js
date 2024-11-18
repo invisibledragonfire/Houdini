6b5457391707e62adcae682c6e95bec00009e232
Felix Prinz <felix.prinz@gintel.com>
2024-11-18 08:27:39 +0100

    add side styles

--- borderWorklet.js
+++ borderWorklet.js
@@ -6,6 +6,8 @@ class BorderPainter {
       "--border-width",
       "--border-radius",
       "--corner-style",
+      "--side-inset",
+      "--side-style",
     ];
   }
 
@@ -20,9 +22,6 @@ class BorderPainter {
 
   paint(ctx, geom, props) {
     // Use `ctx` as if it was a normal canvas
-    console.log(props.get("--corner-style"));
-    console.log(props.getAll("--corner-style"));
-
     const borderRadii = this.parseList(props.getAll("--border-radius")).map(
       (item) => item.value
     );
@@ -30,7 +29,11 @@ class BorderPainter {
     const cornerStyles = this.parseList(
       props.get("--corner-style")[0].split(" ")
     );
-    console.log(cornerStyles);
+
+    const sideInsets = this.parseList(props.getAll("--side-inset")).map(
+      (item) => item.value
+    );
+    const sideStyles = this.parseList(props.get("--side-style")[0].split(" "));
 
     ctx.beginPath();
 
@@ -67,7 +70,9 @@ class BorderPainter {
         nextCorner.x - currentCorner.x,
         nextCorner.y - currentCorner.y,
         borderRadii[i],
-        borderRadii[(i + 1) % corners.length]
+        borderRadii[(i + 1) % corners.length],
+        sideInsets[i],
+        sideStyles[i]
       );
     }
 
@@ -81,9 +86,80 @@ class BorderPainter {
     ctx.stroke();
   }
 
-  drawSide(ctx, start, end, dx, dy, startBorderRadius, endBorderRadius) {
-    // ctx.lineTo(start.x + dx * startBorderRadius, start.y + dy * startBorderRadius);
-    ctx.lineTo(end.x - dx * endBorderRadius, end.y - dy * endBorderRadius);
+  drawSide(
+    ctx,
+    startCorner,
+    endCorner,
+    dx,
+    dy,
+    startBorderRadius,
+    endBorderRadius,
+    sideInset,
+    sideStyle
+  ) {
+    const start = {
+      x: startCorner.x + dx * startBorderRadius,
+      y: startCorner.y + dy * startBorderRadius,
+    };
+
+    const end = {
+      x: endCorner.x - dx * endBorderRadius,
+      y: endCorner.y - dy * endBorderRadius,
+    };
+
+    const inset = {
+      x: -sideInset * dy,
+      y: sideInset * dx,
+    };
+
+    const startMid = {
+      x: start.x + dx * sideInset,
+      y: start.y + dy * sideInset,
+    };
+
+    const endMid = {
+      x: end.x - dx * sideInset,
+      y: end.y - dy * sideInset,
+    };
+
+    const n = Math.abs(dx + dy - 1) + Math.abs(dy);
+
+    switch (sideStyle) {
+      case "inset":
+        ctx.lineTo(start.x + inset.x, start.y + inset.y);
+        ctx.lineTo(end.x + inset.x, end.y + inset.y);
+        ctx.lineTo(end.x, end.y);
+        break;
+
+      case "rounded":
+        ctx.arc(
+          startMid.x,
+          startMid.y,
+          sideInset,
+          ((n + 2) * Math.PI) / 2,
+          ((n + 1) * Math.PI) / 2,
+          true
+        );
+        ctx.lineTo(endMid.x + inset.x, endMid.y + inset.y);
+        ctx.arc(
+          endMid.x,
+          endMid.y,
+          sideInset,
+          ((n + 1) * Math.PI) / 2,
+          ((n + 0) * Math.PI) / 2,
+          true
+        );
+        break;
+
+      case "bevel":
+        ctx.lineTo(startMid.x + inset.x, startMid.y + inset.y);
+        ctx.lineTo(endMid.x + inset.x, endMid.y + inset.y);
+        ctx.lineTo(end.x, end.y);
+        break;
+
+      default:
+        ctx.lineTo(end.x, end.y);
+    }
   }
 
   drawCorner(ctx, corner, dx, dy, borderRadius, cornerStyle) {
