8ab7f1f294dcc5bf944a1ec1a4afe762c7b46ada
Felix Prinz <felix.prinz@gintel.com>
2025-01-28 15:29:59 +0100

    presentation polishing

--- presentation/presentation.js
+++ presentation/presentation.js
@@ -10,11 +10,20 @@ init = function () {
   const borderRadiusSliders = getElements("borderRadiusSlider");
   const borderRadiusCheckboxes = getElements("borderRadiusCheckbox");
 
-  const borderStyleCheckboxes = getElements("borderStyleCheckbox");
   const borderStyleDropdowns = getElements("borderStyleDropdown");
+  const borderStyleCheckboxes = getElements("borderStyleCheckbox");
+
+  const sideInsetSliders = getElements("sideInsetSlider");
+  const sideInsetCheckboxes = getElements("sideInsetCheckbox");
+
+  const sideStyleDropdowns = getElements("sideStyleDropdown");
+  const sideStyleCheckboxes = getElements("sideStyleCheckbox");
 
   let borderRadiusValuesActive = 0;
   let borderStyleValuesActive = 0;
+
+  let sideInsetValuesActive = 0;
+  let sideStyleValuesActive = 0;
 
   updateCorners = function () {
     box.attributeStyleMap.set(
@@ -42,6 +51,32 @@ init = function () {
     dropdown.oninput = updateCornerStyles;
   }
 
+  updateSides = function () {
+    box.attributeStyleMap.set(
+      "--side-inset",
+      [...sideInsetSliders]
+        .splice(0, sideInsetValuesActive + 1)
+        .map((slider) => CSS.px(slider.value).toString())
+        .join(" ")
+    );
+  };
+  for (const slider of sideInsetSliders) {
+    slider.oninput = updateSides;
+  }
+
+  updateSideStyles = function () {
+    box.attributeStyleMap.set(
+      "--side-style",
+      [...sideStyleDropdowns]
+        .splice(0, sideStyleValuesActive + 1)
+        .map((dropdown) => dropdown.value)
+        .join(" ")
+    );
+  };
+  for (const dropdown of sideStyleDropdowns) {
+    dropdown.oninput = updateSideStyles;
+  }
+
   const updateBorderRadiusCheckboxes = (activeValue) => () => {
     borderRadiusValuesActive = activeValue;
     borderRadiusCheckboxes.forEach((checkbox, id) => {
@@ -63,6 +98,33 @@ init = function () {
   borderStyleCheckboxes.forEach((checkbox, id) => {
     checkbox.oninput = updateBorderStyleCheckboxes(id);
   });
+
+  const updateSideInsetCheckboxes = (activeValue) => () => {
+    sideInsetValuesActive = activeValue;
+    sideInsetCheckboxes.forEach((checkbox, id) => {
+      checkbox.checked = id > activeValue ? false : true;
+    });
+    updateSides();
+  };
+  sideInsetCheckboxes.forEach((checkbox, id) => {
+    checkbox.oninput = updateSideInsetCheckboxes(id);
+  });
+
+  const updateSideStyleCheckboxes = (activeValue) => () => {
+    sideStyleValuesActive = activeValue;
+    sideStyleCheckboxes.forEach((checkbox, id) => {
+      checkbox.checked = id > activeValue ? false : true;
+    });
+    updateSideStyles();
+  };
+  sideStyleCheckboxes.forEach((checkbox, id) => {
+    checkbox.oninput = updateSideStyleCheckboxes(id);
+  });
+
+  updateCorners();
+  updateCornerStyles();
+  updateSides();
+  updateSideStyles();
 };
 
 window.onload = init;
